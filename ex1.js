cd68efe45b02228cadb0f6689b4a61b075d9c9fc
KesavanPrasath <k.kesavanprasath@gmail.com>
2026-05-04 13:29:06 +0100

    Implement baggage weight check and removal functions
    Added functionality to check and update baggage weight for overweight alerts.

--- ex1.js
+++ ex1.js
@@ -8,11 +8,33 @@
             //and a button to remove some
             //the remove function should update the weight
             //and let the user know when ok to proceed
-            let check=()=>{
-                        alert('Checking Baggage weight: '+parseInt(document.getElementById('weight').value));
-    //These should only display if it is actually overweight
-                        document.getElementById('removed').style='display:block;'
-                        document.getElementById('remB').style='display:block;'
-}
-                        let remove=()=>alert('Removing weight: '+parseInt(document.getElementById('removed').value))
+           let check = () => {
+  let weight = parseInt(document.getElementById('weight').value);
+
+  if (weight > 15) {
+    alert('Your baggage is overweight! (' + weight + 'kg). Please remove some items.');
+    document.getElementById('removed').style = 'display:block;';
+    document.getElementById('remB').style = 'display:block;';
+  } else {
+    alert('Your baggage is fine! (' + weight + 'kg). You are good to go ✅');
+    document.getElementById('removed').style = 'display:none;';
+    document.getElementById('remB').style = 'display:none;';
+  }
+};
+
+let remove = () => {
+  let currentWeight = parseInt(document.getElementById('weight').value);
+  let removeAmount = parseInt(document.getElementById('removed').value);
+  let newWeight = currentWeight - removeAmount;
+
+  document.getElementById('weight').value = newWeight;
+
+  if (newWeight <= 15) {
+    alert('Weight updated to ' + newWeight + 'kg. You are OK to proceed ✅');
+    document.getElementById('removed').style = 'display:none;';
+    document.getElementById('remB').style = 'display:none;';
+  } else {
+    alert('Still overweight! Current weight: ' + newWeight + 'kg. Remove more!');
+  }
+};
                         //Test edit no. 2;
