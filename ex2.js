82ebaf5f3474bc764ab9ca80d347b1f5cacdfe2b
KesavanPrasath <k.kesavanprasath@gmail.com>
2026-05-04 14:21:49 +0100

    Implement Euler problem solutions in ex2.js

--- ex2.js
+++ ex2.js
@@ -2,6 +2,25 @@
 //https://projecteuler.net/problem=1
 //ex2 should output the sum of all multiples of a or b less than n
 
-let euler1=()=>alert();
+let euler1 = () => {
+  let sum = 0;
+  for (let i = 1; i < 1000; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i;
+    }
+  }
+  alert('Sum of multiples of 3 or 5 below 1000 is: ' + sum);
+}
 
-let eulerCustom=()=>alert()
+let eulerCustom = () => {
+  let a = parseInt(document.getElementById('A').value);
+  let b = parseInt(document.getElementById('B').value);
+  let n = parseInt(document.getElementById('N').value);
+  let sum = 0;
+  for (let i = 1; i < n; i++) {
+    if (i % a === 0 || i % b === 0) {
+      sum += i;
+    }
+  }
+  alert('Sum of multiples of ' + a + ' or ' + b + ' below ' + n + ' is: ' + sum);
+}
