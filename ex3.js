d93226da560cffedd5c27459b4e4516cd54b0184
KesavanPrasath <k.kesavanprasath@gmail.com>
2026-05-04 14:27:35 +0100

    Implement functions to calculate sums of multiples

--- ex3.js
+++ ex3.js
@@ -1,24 +1,51 @@
-let listEuler1=(a,b,l)=>0;//obviously wrong - compute the sum of multiples of a or b in list l
-let listEuler2=(a,l)=>0;//obviously wrong - compute the sum of multiples in list a found in list l
-let listEuler3=(a,l)=>0;//obviously wrong - compute the sum of multiples in list a found in list l
-
-let eulerlist=()=>
-{
-    a=2//can un-hardcode
-    b=3
-    l=[1,2,3,4,5,6,7,9,10,10,10]  //document.getElementById('l').value.split(" ").map((x=>parseInt(x)); // if l is space separated numbers
-    //call listEuler1 and alert. - compute the sum of multiples of a or b in list l
-}
-let euler2Lists=()=>
-{
-    a=[2,3]//can un-hardcode
-    l=[1,2,3,4,5,6,7,9,10,10,10]
-    //call listEuler2 and alert.
+let listEuler1 = (a, b, l) => {
+  let sum = 0;
+  for (let i = 0; i < l.length; i++) {
+    if (l[i] % a === 0 || l[i] % b === 0) {
+      sum += l[i];
+    }
+  }
+  return sum;
 }
 
-let euler2Lists1=()=>
-{
-    a=[2,3,5]//can un-hardcode
-    l=[1,2,3,4,5,6,7,9,10,10,10]
-    //call listEuler3 and alert.
+let listEuler2 = (a, l) => {
+  let sum = 0;
+  for (let i = 0; i < l.length; i++) {
+    if (l[i] % a[0] === 0 || l[i] % a[1] === 0) {
+      sum += l[i];
+    }
+  }
+  return sum;
+}
+
+let listEuler3 = (a, l) => {
+  let sum = 0;
+  for (let i = 0; i < l.length; i++) {
+    for (let j = 0; j < a.length; j++) {
+      if (l[i] % a[j] === 0) {
+        sum += l[i];
+        break;
+      }
+    }
+  }
+  return sum;
+}
+
+let eulerlist = () => {
+  a = 2;
+  b = 3;
+  l = [1, 2, 3, 4, 5, 6, 7, 9, 10, 10, 10];
+  alert('Sum of multiples of ' + a + ' or ' + b + ' in list: ' + listEuler1(a, b, l));
+}
+
+let euler2Lists = () => {
+  a = [2, 3];
+  l = [1, 2, 3, 4, 5, 6, 7, 9, 10, 10, 10];
+  alert('Sum of multiples of list a in list l: ' + listEuler2(a, l));
+}
+
+let euler2Lists1 = () => {
+  a = [2, 3, 5];
+  l = [1, 2, 3, 4, 5, 6, 7, 9, 10, 10, 10];
+  alert('Sum of multiples of list a in list l: ' + listEuler3(a, l));
 }
