2a163dd1443758eeb00bf6afeea8a4e69c68da6b
KesavanPrasath <k.kesavanprasath@gmail.com>
2026-05-20 13:52:03 +0100

    Click Counter web component
    Implemented a ClickCounter web component that tracks button clicks.

--- ex7.js
+++ ex7.js
@@ -1,3 +1,19 @@
 // create a web component to achieve a task of your choice.
 
 //example at https://jsfiddle.net/lairdp/o475u9zL
+
+<click-counter></click-counter>
+
+<script>
+class ClickCounter extends HTMLElement {
+  connectedCallback() {
+    this._n = 0;
+    this.innerHTML = `<button>Clicked 0 times</button>`;
+    this.querySelector('button').addEventListener('click', () => {
+      this._n++;
+      this.querySelector('button').textContent = `Clicked ${this._n} times`;
+    });
+  }
+}
+customElements.define('click-counter', ClickCounter);
+</script>
