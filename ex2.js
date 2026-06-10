74d9b8cd529f24ac86b7e626da35ea4f0ff2c658
KesavanPrasath <k.kesavanprasath@gmail.com>
2026-06-10 13:04:51 +0100

    Refactor functions to calculate sums of multiples

--- ex2.js
+++ ex2.js
@@ -1,26 +1,26 @@
-//redefine these - ex1 should output the solution to project Euler Q1
-//https://projecteuler.net/problem=1
-//ex2 should output the sum of all multiples of a or b less than n
-
-let euler1 = () => {
+function showResult() {
   let sum = 0;
+
   for (let i = 1; i < 1000; i++) {
     if (i % 3 === 0 || i % 5 === 0) {
       sum += i;
     }
   }
-  alert('Sum of multiples of 3 or 5 below 1000 is: ' + sum);
+
+  alert("Sum is: " + sum);
 }
 
-let eulerCustom = () => {
-  let a = parseInt(document.getElementById('A').value);
-  let b = parseInt(document.getElementById('B').value);
-  let n = parseInt(document.getElementById('N').value);
-  let sum = 0;
-  for (let i = 1; i < n; i++) {
-    if (i % a === 0 || i % b === 0) {
-      sum += i;
+function sumMultiples() {
+    const a = parseInt(document.getElementById('aInput').value);
+    const b = parseInt(document.getElementById('bInput').value);
+    const n = parseInt(document.getElementById('nInput').value);
+    
+    let sum = 0;
+    for (let i = 1; i < n; i++) {
+        if (i % a === 0 || i % b === 0) {
+            sum += i;
+        }
     }
-  }
-  alert('Sum of multiples of ' + a + ' or ' + b + ' below ' + n + ' is: ' + sum);
+    
+    alert("Sum of multiples of " + a + " or " + b + " below " + n + " is: " + sum);
 }
